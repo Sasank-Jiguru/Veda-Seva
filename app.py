0578808619b723ae91c864cbb1295c92ce7b99b6
Sasank <sasankjiguru2006@gmail.com>
2026-05-23 12:22:14 +0530

    last fix

--- app.py
+++ app.py
@@ -389,5 +389,8 @@ def add_reminder():
 if __name__ == '__main__':
 
     init_db()
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
